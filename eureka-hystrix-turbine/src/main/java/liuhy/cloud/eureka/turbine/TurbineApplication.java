26c3900fec29b1598cf1a47bca352856f5615eb6
liuhy <568656253@qq.com>
2018-07-22 23:08:25 +0800

    123123

--- eureka-hystrix-turbine/src/main/java/liuhy/cloud/eureka/turbine/TurbineApplication.java
+++ eureka-hystrix-turbine/src/main/java/liuhy/cloud/eureka/turbine/TurbineApplication.java
@@ -1,11 +1,12 @@
-package liuhy.cloud.eureka.turbine
+package liuhy.cloud.eureka.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
-@EnableTurbine
+//@EnableTurbine注解包含了@EnableDiscoveryClient注解
 @SpringBootApplication
+@EnableTurbine
 public class TurbineApplication {
   public static void main(String[] args) {
     SpringApplication.run(TurbineApplication.class, args);
