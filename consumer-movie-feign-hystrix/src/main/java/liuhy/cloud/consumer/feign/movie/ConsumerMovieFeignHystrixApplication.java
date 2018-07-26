80b789dabd66974de3b23d57586484f91b1f6e59
liuhuayun <568656253@qq.com>
2018-07-26 18:01:15 +0800

    123

--- consumer-movie-feign-hystrix/src/main/java/liuhy/cloud/consumer/feign/movie/ConsumerMovieFeignHystrixApplication.java
+++ consumer-movie-feign-hystrix/src/main/java/liuhy/cloud/consumer/feign/movie/ConsumerMovieFeignHystrixApplication.java
@@ -2,11 +2,15 @@ package liuhy.cloud.consumer.feign.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -16,6 +20,25 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableHystrixDashboard
 public class ConsumerMovieFeignHystrixApplication {
 	
+  /**
+   * spring cloud2 hystrix没有/actuator/hystrix.stream路径解决
+   * @return
+   */
+  @Bean
+  public ServletRegistrationBean getServlet(){
+	/*
+	 * 访问http://localhost:8010//hystrix，输入框输入以下地址
+	 * http://localhost:8010/actuator/hystrix.stream
+	 * 
+	 */     
+	HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+     ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+     registrationBean.setLoadOnStartup(1);
+     registrationBean.addUrlMappings("/hystrix.stream");
+     registrationBean.setName("HystrixMetricsStreamServlet");
+     return registrationBean;
+  }
+  
   public static void main(String[] args) {
     SpringApplication.run(ConsumerMovieFeignHystrixApplication.class, args);
   }
