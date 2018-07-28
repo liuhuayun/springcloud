3935b19040ff76658fd8a84b914cbfcdc88537b8
liuhy <568656253@qq.com>
2018-07-28 21:48:20 +0800

    123

--- springcloud-config/src/main/java/liuhy/cloud/config/SpringCloudConfigApplication.java
+++ springcloud-config/src/main/java/liuhy/cloud/config/SpringCloudConfigApplication.java
@@ -5,6 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+http请求地址和资源文件映射如下:
+
+/{application}/{profile}[/{label}]
+/{application}-{profile}.yml
+/{label}/{application}-{profile}.yml
+/{application}-{profile}.properties
+/{label}/{application}-{profile}.properties
+ 
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableConfigServer
